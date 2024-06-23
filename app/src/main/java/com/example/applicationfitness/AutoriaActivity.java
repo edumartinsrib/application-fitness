8985649bfcfb861380a202b559cb2e3b3c421c18
edumartinsrib <edumartinsrib@live.com>
2024-06-23 19:11:50 -0300

    feat: include view autoria and activity

---
+++ app/src/main/java/com/example/applicationfitness/AutoriaActivity.java
@@ -0,0 +1,14 @@
+package com.example.applicationfitness;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AutoriaActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autoria);
+    }
+}
